aa2f672e2d5fbfafa380d84a3fa51071ac5dbdaa
satoshi.fujita <not_rishi@yahoo.co.jp>
2022-05-19 02:41:12 +0000

    my_calendar

---
+++ db/migrate/20220518080556_create_blogs.rb
@@ -0,0 +1,11 @@
+class CreateBlogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :blogs do |t|#ここから
+      t.string :title
+      t.text :content
+      t.datetime :start_time#ここまでを追加
+      
+      t.timestamps
+    end
+  end
+end
